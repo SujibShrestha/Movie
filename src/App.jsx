63c32bf6cbcc4c93045dd42ac3c927a88a678dbb
SujibShrestha <sujeeb98@gmail.com>
2025-06-05 11:08:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,130 @@
+import Search from "./component/Search";
+import { useState, useEffect } from "react";
+import Spinner from "./component/Spinner";
+import MovieCard from "./component/MovieCard";
+import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearch } from "./appwrite";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = "") => {
+    setIsLoading();
+    setErrorMessage("");
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new error("Fail to fetch Movie data");
+      }
+      const data = await response.json();
+
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearch(query, data.results[0]);
+      }
+    } catch (error) {
+      console.log("Error Fetching movies :" + error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+       setTrendingMovies(movies);
+    } catch (error) {
+      console.log("Error fetching trending movies");
+     
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero-Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without Hassle
+          </h1>{" "}
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie,index)=>(
+                <li key={movie.$id}>
+                <p>{index+1}</p>
+                <img src={movie.poster_url} alt="MoviePoster" />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+            <p className="text-white flex justify-center">
+              <Spinner />
+            </p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/component/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div className="search">
+      <div>
+        <img src="public\Vector.svg" alt="Search-icon" />
+        <input type="text"
+        placeholder="Enter movie name" 
+        value={searchTerm}
+        onChange={(e)=> setSearchTerm(e.target.value)}/>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
